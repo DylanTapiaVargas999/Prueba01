27f4830eb24d645aa7f6423b6482c4b355b1e9cc
Dylan Tapia <dt2021072630@virtual.upt.pe>
2024-08-30 22:35:11 -0500

    prueba de action8

--- biblioteca/Libraries/Core/Load.php
+++ biblioteca/Libraries/Core/Load.php
@@ -1,15 +1,24 @@
 <?php
-$controllerFile = "Controller/" . $controller . ".php";
+// Obtén el directorio actual del script
+$currentDir = __DIR__;
+
+// Ruta relativa al directorio actual
+$controllerFile = $currentDir . '/../Controller/' . $controller . '.php';
+
+// Verifica si el archivo del controlador existe
 if (file_exists($controllerFile)) {
     require_once($controllerFile);
     $controller = new $controller();
+    
+    // Verifica si el método existe en el controlador
     if (method_exists($controller, $methop)) {
         $controller->{$methop}($params);
     } else {
-        require_once("Controller/Error.php");
+        // Ruta ajustada al archivo Error.php
+        require_once($currentDir . '/../Controller/Error.php');
     }
 } else {
-    require_once("Controller/Error.php");
+    // Ruta ajustada al archivo Error.php
+    require_once($currentDir . '/../Controller/Error.php');
 }
-
 ?>
