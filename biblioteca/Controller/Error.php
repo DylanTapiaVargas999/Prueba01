927374487e09308d6c899d14c9ba7855b41e539c
Dylan Tapia <dt2021072630@virtual.upt.pe>
2024-09-27 19:22:00 -0500

    v1

--- biblioteca/Controller/Error.php
+++ biblioteca/Controller/Error.php
@@ -1,14 +1,16 @@
 <?php
-class Errors extends Controllers {
-    public function __construct() {
-        parent::__construct();
-    }
+    class Errors extends Controllers{
+        public function __construct()
+        {
+            parent::__construct();
 
-    public function notFound() {
-        $this->views->getView($this, "error");
+        }
+        public function notFound()
+        {
+            $this->views->getView($this, "error");
+        }
     }
-}
-
-$notFound = new Errors();
-$notFound->notFound();
-?>
+    $notFound = new Errors();
+    $notFound ->notFound();
+    ?>
+    
--- biblioteca/Libraries/Core/Load.php
+++ biblioteca/Libraries/Core/Load.php
@@ -1,21 +1,15 @@
 <?php
-$currentDir = __DIR__;
-$controllerFile = $currentDir . '/../Controller/' . $controller . '.php';
-$errorFile = $currentDir . '/../Controller/Error.php';
-
-echo "Attempting to load controller file: $controllerFile\n";
-echo "Attempting to load error file: $errorFile\n";
-
+$controllerFile = "Controller/" . $controller . ".php";
 if (file_exists($controllerFile)) {
     require_once($controllerFile);
     $controller = new $controller();
-    
     if (method_exists($controller, $methop)) {
         $controller->{$methop}($params);
     } else {
-        require_once($errorFile);
+        require_once("Controller/Error.php");
     }
 } else {
-    require_once($errorFile);
+    require_once("Controller/Error.php");
 }
+
 ?>
